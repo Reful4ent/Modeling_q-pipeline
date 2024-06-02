14b29dea608a2ab9087f2d3b0dca93595d3b6480
Reful4ent <dima2323fr3@gmail.com>
2024-06-02 16:43:10 +0700

    Troubles

--- Modeling_DeliveryService.ConsoleV/Program.cs
+++ Modeling_DeliveryService.ConsoleV/Program.cs
@@ -1,9 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Modeling_DeliveryService.ConsoleV.Model;
-using Modeling_q_pipeline.Model;
-
 Console.WriteLine("Hello, World!");
-IStatistics statistics = new Statistics();
-Service service = new Service(statistics);
-service.Start(1000,5);
